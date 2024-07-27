e84588c66958376a92b2995d426354e45a6182af
duanepm <duane3pm@gmail.com>
2024-07-27 21:51:25 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,10 @@ while :
 do
     read -p "---> " -a command_array
 
+    if [ ${#command_array[@]} -eq 0 ]; then
+        echo "No command entered. Please enter a command."
+        continue
+    fi
+
     ./file_manager.out "${command_array[@]}" 
 done
